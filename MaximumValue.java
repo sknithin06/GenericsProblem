2280d74c26267eebbf25bbc664e50c72214c580c
sknithin06 <nithinsk939@gmail.com>
2021-07-19 00:10:03 +0530

    checking the largest value in float

--- MaximumValue.java
+++ MaximumValue.java
@@ -15,7 +15,7 @@ public class MaximumValue {
 				System.out.println("Maximum value is b:"+max);
 			}
 		}
-		if (c.compareTo(a) > 0) {
+		else if (c.compareTo(a) > 0) {
 			if (c.compareTo(b) > 0) {
 				Integer max = c;
 				System.out.println("Maximum value is c:"+max);
@@ -26,9 +26,35 @@ public class MaximumValue {
 			System.out.println("Maximum value is a:"+max);
 		}
 	}
+	static void numMaximumFloat() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your First Number:");  
+		Float a = sc.nextFloat();
+		System.out.print("Enter your Second Number:");
+		Float b = sc.nextFloat();
+		System.out.print("Enter your Third Number:");
+		Float c = sc.nextFloat();
+		if (b.compareTo(a) > 0) {
+			if (b.compareTo(c) > 0) {
+				Float max = b;
+				System.out.println("Maximum value is b:"+max);
+			}
+		}
+		else if (c.compareTo(a) > 0) {
+			if (c.compareTo(b) > 0) {
+				Float max = c;
+				System.out.println("Maximum value is c:"+max);
+			}
+		}
+		else {
+			 Float max = a;
+			System.out.println("Maximum value is a:"+max);
+		}
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generic Problem");
 		
 		numMaximumInt();
+		numMaximumFloat();
 	}
 }
